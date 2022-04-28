422ae65fa6778bec683370533a9107871cbcd6ce
SalmaHoss <salmahossam639@gmail.com>
2022-04-28 21:25:29 +0200

    Models Creation

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
 
 //2
 
