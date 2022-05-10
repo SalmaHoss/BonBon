a3eedc40c50f07519843788d5ab01e3cb3bb9922
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-05-10 18:02:01 +0200

    Edit Roles

--- Controllers/UserProfileController.cs
+++ Controllers/UserProfileController.cs
@@ -11,9 +11,7 @@ namespace AngularProject.Controllers
     [ApiController]
     public class UserProfileController : ControllerBase
     {
-        /// <summary>
-        /// not tested
-        /// </summary>
+        
 
         private ApplicationDbContext context;
         private UserManager<User> userManager;
